2f028cf99d736d03fddb58739bd2b050d7aa8650
Alex Hernandez <alexjh221@gmail.com>
2025-06-12 23:10:51 -0500

    add routing to frontend

---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div>Welcome to the About Us Page</div>
+};
+
+export default About;
---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="text-2xl font-bol text-center mt-10">Welcome to the Home Page</div>
+};
+
+export default Home;
